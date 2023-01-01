b60fdb5050aa55995d4ef677c52034e47e16b274
Vishal Pandey <vishalmimit999@gmail.com>
2023-01-01 00:08:03 -0500

    AutoComplete txt

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 private ListView listView1;
 private Spinner spinnerId;
+private AutoCompleteTextView autotxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +24,10 @@ private Spinner spinnerId;
 
         listView1 = findViewById(R.id.listView1);
         spinnerId = findViewById(R.id.spinnerId);
+        autotxt = findViewById(R.id.autoComplete);
         ArrayList<String> arr = new ArrayList<>();
         ArrayList<String> arrSpinner = new ArrayList<>();
+        ArrayList<String> autoComplete = new ArrayList<>();
 
         arr.add("Vishal");
         arr.add("Pandey");
@@ -68,5 +72,19 @@ private Spinner spinnerId;
 
         ArrayAdapter<String> spinadapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,arrSpinner);
         spinnerId.setAdapter(spinadapter);
+
+        autoComplete.add("C++");
+        autoComplete.add("JAVA");
+        autoComplete.add("PHP");
+        autoComplete.add("Pythone");
+        autoComplete.add("webcam");
+        autoComplete.add("c#");
+        autoComplete.add("JAVA Script");
+        autoComplete.add("Node Js");
+        autoComplete.add("React Js");
+
+        ArrayAdapter<String> autoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,autoComplete);
+        autotxt.setAdapter(autoAdapter);
+        autotxt.setThreshold(1);
     }
 }
